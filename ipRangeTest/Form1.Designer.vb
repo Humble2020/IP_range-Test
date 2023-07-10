6f0c53e1fc5f84e95a716d6b7e662a2de21c8f14
JOSEPH ALAKPA <alakpauzezi2017@gmail.com>
2023-07-10 14:19:43 +0100

    added the folders and files

---
+++ ipRangeTest/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(460, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(174, 303)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(71, 148)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(71, 82)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "start"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "end"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(71, 108)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(71, 177)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(46, 257)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(148, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "split start and end range"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(25, 231)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(184, 20)
+        Me.TextBox3.TabIndex = 8
+        Me.TextBox3.Text = "120.021.332.564-221.203.453.611"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(63, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(108, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "ipRange loop"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(227, 12)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(227, 303)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(63, 41)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(108, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "stop ipRange loop"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(646, 329)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ ipRangeTest/Form1.vb
@@ -0,0 +1,191 @@
+﻿Imports System.Threading
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'start ip range
+        Timer1.Start()
+    End Sub
+    Sub cjhg()
+        Dim bbb As String = Me.TextBox1.Text 'starting range
+        Dim ghb() As String = Split(bbb, ".")
+        Dim kl = CInt(ghb(0))
+        Dim kl2 = CInt(ghb(1))
+        Dim kl3 = CInt(ghb(2))
+        Dim kl4 = CInt(ghb(3))
+        Dim bbb2 As String = Me.TextBox2.Text 'ending range
+        Dim ghb2() As String = Split(bbb2, ".")
+        Dim kll = CInt(ghb2(0))
+        Dim kll2 = CInt(ghb2(1))
+        Dim kll3 = CInt(ghb2(2))
+        Dim kll4 = CInt(ghb2(3))
+        ' ListBox1.Items.Add("First: " & kl)
+        ' ListBox1.Items.Add("Second: " & kl2)
+        ' ListBox1.Items.Add("Third: " & kl3)
+        ' ListBox1.Items.Add("Fourth: " & kl4)
+        ' ListBox1.Items.Add(Environment.NewLine)
+        ' ListBox1.Items.Add("First: " & kll)
+        ' ListBox1.Items.Add("Second: " & kll2)
+        ' ListBox1.Items.Add("Third: " & kll3)
+        ' ListBox1.Items.Add("Fourth: " & kll4)
+        ' ListBox1.Items.Add(Environment.NewLine)
+        For x1 As Integer = kl To kll
+            For x2 As Integer = kl2 To kll2
+                For x3 As Integer = kl3 To kll3
+                    For x4 As Integer = kl4 To kll4
+                        If kl = kll AndAlso kl2 = kll2 AndAlso kl3 = kll3 AndAlso kl4 = kll4 Then
+                            'Exit Sub
+                        End If
+                        bnd = Nothing
+                        bnd = x1 & "." & x2 & "." & x3 & "." & x4 & Environment.NewLine
+                        countt += 1
+                        lp = countt
+                        Me.Invoke(New MethodInvoker(AddressOf addlio))
+                    Next
+                Next
+            Next
+        Next
+    End Sub
+    Dim kf As New ListBox
+    Dim lp As Integer
+    Dim bnd As String
+    Dim countt As Integer = 0
+    Sub addlio()
+        'If Not bb.Text = String.Empty AndAlso Not ListBox1.Items.Contains(bb) Then
+        Me.TextBox4.AppendText(bnd)
+        Me.Label2.Text = lp
+        ' End If
+    End Sub
+    Private Sub method_2()
+        Dim index As Integer = 0
+        Do While (index < 1) ''change number to length of iprange
+            ' Dim str As String = Me.rtbIPRanges.Lines(index).Trim
+            Dim str2 As String = Me.TextBox1.Text
+            Dim str3 As String = Me.TextBox2.Text
+            Dim strArray() As String = Split(str2, ".")
+            Dim strArray2() As String = Split(str3, ".")
+           Do While True
+                If (Integer.Parse(strArray(0)) > Integer.Parse(strArray2(0))) Then
+                    index += 1
+                    Exit Do
+                End If
+                Do While True
+                    If (Integer.Parse(strArray(1)) > Integer.Parse(strArray2(1))) Then
+                        strArray(0) = (Integer.Parse(strArray(0)) + 1).ToString
+                        strArray(1) = "0"
+                        strArray(2) = "0"
+                        strArray(3) = "0"
+                        Exit Do
+                    End If
+                    Do While True
+                        'Dim info As SSH_Info
+                        Dim strArray3 As String()
+                        If (Integer.Parse(strArray(2)) > Integer.Parse(strArray2(2))) Then
+                            strArray(1) = (Integer.Parse(strArray(1)) + 1).ToString
+                            strArray(2) = "0"
+                            strArray(3) = "0"
+                            Exit Do
+                        End If
+                        If (((strArray(0) = strArray2(0)) AndAlso (strArray(1) = strArray2(1))) AndAlso (strArray(2) = strArray2(2))) Then
+                            Do While (Integer.Parse(strArray(3)) <= Integer.Parse(strArray2(3)))
+                                strArray3 = New String() {strArray(0), ".", strArray(1), ".", strArray(2), ".", strArray(3)}
+                              If String.Concat(strArray3) >= str3 Then
+                                    Exit Sub
+                                End If
+                                bnd = String.Concat(strArray3) & Environment.NewLine
+                                countt += 1
+                                lp = countt
+                                Me.Invoke(New MethodInvoker(AddressOf addlio))
+                                strArray(3) = (Integer.Parse(strArray(3)) + 1).ToString
+                            Loop
+                        Else
+                            Dim i As Integer = 0
+                            Do While (i <= &HFF)
+                                strArray3 = New String() {strArray(0), ".", strArray(1), ".", strArray(2), ".", i.ToString}
+                               If String.Concat(strArray3) >= str3 Then
+                                    Exit Sub
+                                End If
+                                bnd = String.Concat(strArray3) & Environment.NewLine
+                                countt += 1
+                                lp = countt
+                                Me.Invoke(New MethodInvoker(AddressOf addlio))
+                                i += 1
+                            Loop
+                        End If
+                        strArray(2) = (Integer.Parse(strArray(2)) + 1).ToString
+                        strArray(3) = "0"
+                    Loop
+                Loop
+            Loop
+        Loop
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim bbb As String = Me.TextBox1.Text 'starting range
+        Dim ghb() As String = Split(bbb, ".")
+        Dim kl = CInt(ghb(0))
+        Dim kl2 = CInt(ghb(1))
+        Dim kl3 = CInt(ghb(2))
+        Dim kl4 = CInt(ghb(3))
+        Dim bbb2 As String = Me.TextBox2.Text 'ending range
+        Dim ghb2() As String = Split(bbb2, ".")
+        Dim kll = CInt(ghb2(0))
+        Dim kll2 = CInt(ghb2(1))
+        Dim kll3 = CInt(ghb2(2))
+        Dim kll4 = CInt(ghb2(3))
+        ' ListBox1.Items.Add("First: " & kl)
+        ' ListBox1.Items.Add("Second: " & kl2)
+        ' ListBox1.Items.Add("Third: " & kl3)
+        ' ListBox1.Items.Add("Fourth: " & kl4)
+        ' ListBox1.Items.Add(Environment.NewLine)
+        ' ListBox1.Items.Add("First: " & kll)
+        ' ListBox1.Items.Add("Second: " & kll2)
+        ' ListBox1.Items.Add("Third: " & kll3)
+        ' ListBox1.Items.Add("Fourth: " & kll4)
+        ' ListBox1.Items.Add(Environment.NewLine)
+        For x1 As Integer = kl To kll
+            For x2 As Integer = kl2 To kll2
+                For x3 As Integer = kl3 To kll3
+                    For x4 As Integer = kl4 To kll4
+                        If kl = kll AndAlso kl2 = kll2 AndAlso kl3 = kll3 AndAlso kl4 = kll4 Then
+                            Timer1.Stop()
+                            Exit Sub
+                        End If
+                        ListBox1.Items.Add(x1 & "." & x2 & "." & x3 & "." & x4)
+                        ListBox1.Refresh()
+                        Label2.Text = ListBox1.Items.Count
+                        Label2.Refresh()
+                    Next
+                Next
+            Next
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'stop ip range
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim startip As String = Nothing
+        Dim stopip As String = Nothing
+        Dim bnh As String = Me.TextBox3.Text
+        Dim sptyo() As String = Split(bnh, "-")
+        startip = (sptyo(0))
+        stopip = (sptyo(1))
+        Me.TextBox1.Text = startip
+        Me.TextBox2.Text = stopip
+    End Sub
+
+    Dim klb As Thread
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        klb = New Thread(New ThreadStart(AddressOf method_2)) 'cjhg
+        klb.IsBackground = True
+        klb.Start()
+    End Sub
+
+   Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If klb.IsAlive Then
+            klb.Suspend()
+        End If
+    End Sub
+End Class
